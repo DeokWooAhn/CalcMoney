f2712ae0fe5e962a2ad9668a28f8caf3d90f37b6
DoekWooAhn <fjrlvkdlxj@naver.com>
2025-08-30 00:02:24 +0900

    상태바 색상 변경

--- presentation/src/main/java/com/example/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/main/MainActivity.kt
@@ -1,17 +1,10 @@
 package com.example.presentation.main
 
-import android.graphics.Color.BLACK
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.presentation.ui.theme.CalcMoneyTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,7 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge(statusBarStyle = SystemBarStyle.dark(BLACK))
+        enableEdgeToEdge()
         setContent {
             MainScreen()
         }

302365676c11374aaf2d7f18420a3a8ac6009a4a
dwahn <fjrlvkdlxj@naver.com>
2025-09-02 17:07:15 +0900

    아이콘 이미지 추가

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
 
     implementation(libs.androidx.runtime)
 
+    implementation(libs.androidx.core.splashscreen)
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
--- presentation/src/main/java/com/example/presentation/main/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/main/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.presentation.ui.theme.CalcMoneyTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {

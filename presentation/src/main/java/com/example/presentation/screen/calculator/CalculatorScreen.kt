52b16ff3cd71e5305a3e1fb9ae131e0a2785ebab
dwahn <fjrlvkdlxj@naver.com>
2025-09-21 18:47:15 +0900

    계산기 ui 추가중

--- presentation/src/main/java/com/example/presentation/screen/calculator/CalculatorScreen.kt
+++ presentation/src/main/java/com/example/presentation/screen/calculator/CalculatorScreen.kt
@@ -1,22 +1,130 @@
 package com.example.presentation.screen.calculator
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.presentation.R
+import kotlin.math.exp
 
 @Composable
-fun CalculatorScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(text = "홈 화면")
+fun CalculatorScreen(
+    modifier: Modifier = Modifier
+) {
+    var expression by remember { mutableStateOf("0") }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color.LightGray)
+            .padding(8.dp),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(8.dp),
+            contentAlignment = Alignment.BottomEnd
+        ) {
+            Text(
+                text = expression,
+                color = Color.White,
+                fontSize = 48.sp,
+                textAlign = TextAlign.End,
+                maxLines = 2,
+            )
+        }
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            val buttonModifier = Modifier
+                .weight(1f)
+                .aspectRatio(1f)
+
+            val rows = listOf(
+                listOf("AC", "⌫", stringResource(R.string.divide)),
+                listOf("7", "8", "9", "x"),
+                listOf("4", "5", "6", "-"),
+                listOf("1", "2", "3", "+"),
+                listOf("0", ".", "=", ""),
+            )
+
+            rows.forEach { row ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    row.forEach { label ->
+                        if (label.isNotEmpty()) {
+                            CalculatorButton(
+                                text = label,
+                                modifier = buttonModifier
+                            ) {
+                                expression =
+                                    if (expression == "0") label else expression + label
+                            }
+                        } else {
+                            Spacer(modifier = buttonModifier)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
-@Preview
+@Composable
+fun CalculatorButton(
+    text: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .background(Color.DarkGray, shape = MaterialTheme.shapes.medium)
+            .clickable { onClick() },
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            fontSize = 24.sp,
+            color = Color.White
+        )
+    }
+}
+
+@Preview(showBackground = true)
 @Composable
 fun CalculatorScreenPreview() {
-    CalculatorScreen()
+    Surface {
+        CalculatorScreen()
+    }
 }

c9d24358379c53974bfc3582f4c94059b2602aff
dwahn <fjrlvkdlxj@naver.com>
2026-02-28 22:08:12 +0900

    CalculatorViewModel test 코드 작성

--- presentation/src/test/java/com/ahn/presentation/ui/screen/calculator/CalculatorViewModelTest.kt
+++ presentation/src/test/java/com/ahn/presentation/ui/screen/calculator/CalculatorViewModelTest.kt
@@ -1,5 +1,6 @@
 package com.ahn.presentation.ui.screen.calculator
 
+import app.cash.turbine.test
 import com.ahn.domain.usecase.CalculatorEngine
 import io.kotest.core.spec.IsolationMode
 import io.kotest.core.spec.style.BehaviorSpec
@@ -65,25 +66,23 @@ class CalculatorViewModelTest : BehaviorSpec({
     Given("'123'이 입력된 상태에서") {
 
         When("AC를 누르면") {
-            val viewModel = CalculatorViewModel(calculatorEngine)
-            listOf("1", "2", "3").forEach {
-                viewModel.processIntent(
-                    CalculatorContract.Intent.Input(CalculatorToken.Number(it))
-                )
-            }
+                listOf("1", "2", "3").forEach {
+                    viewModel.processIntent(
+                        CalculatorContract.Intent.Input(CalculatorToken.Number(it))
+                    )
+                }
             viewModel.processIntent(CalculatorContract.Intent.Clear)
 
             Then("expression은 비어있어야 한다") {
-                viewModel.state.value.expression shouldBe ""
+                viewModel.container.stateFlow.value.expression shouldBe ""
             }
 
             Then("커서 위치는 0이어야 한다") {
-                viewModel.state.value.cursorPosition shouldBe 0
+                viewModel.container.stateFlow.value.cursorPosition shouldBe 0
             }
         }
 
         When("삭제(⌫)를 누르면") {
-            val viewModel = CalculatorViewModel(calculatorEngine)
             listOf("1", "2", "3").forEach {
                 viewModel.processIntent(
                     CalculatorContract.Intent.Input(CalculatorToken.Number(it))
@@ -92,17 +91,16 @@ class CalculatorViewModelTest : BehaviorSpec({
             viewModel.processIntent(CalculatorContract.Intent.Delete)
 
             Then("마지막 글자가 지워져 '12'가 되어야 한다") {
-                viewModel.state.value.expression shouldBe "12"
+                viewModel.container.stateFlow.value.expression shouldBe "12"
             }
         }
     }
 
     Given("'10+20'이 입력된 상태에서") {
+        every { calculatorEngine.calculate(any()) } returns "0"
+        every { calculatorEngine.calculate("10+20") } returns "30"
 
         When("=을 누르면") {
-            every { calculatorEngine.calculate("10+20") } returns "30"
-
-            val viewModel = CalculatorViewModel(calculatorEngine)
             "10+20".forEach { ch ->
                 val token = when {
                     ch.isDigit() -> CalculatorToken.Number(ch.toString())
@@ -113,7 +111,33 @@ class CalculatorViewModelTest : BehaviorSpec({
             viewModel.processIntent(CalculatorContract.Intent.Calculate)
 
             Then("결과 '30'이 expression에 표시되어야 한다") {
-                viewModel.state.value.expression shouldBe "30"
+                viewModel.container.stateFlow.value.expression shouldBe "30"
+            }
+        }
+    }
+
+    Given("계산 결과가 에러일 때") {
+        every { calculatorEngine.calculate(any()) } returns "Error"
+
+        When("=을 누르면") {
+            viewModel.processIntent(
+                CalculatorContract.Intent.Input(CalculatorToken.Number("1"))
+            )
+            viewModel.processIntent(CalculatorContract.Intent.Calculate)
+
+            Then("에러 상태가 되어야 한다") {
+                viewModel.container.stateFlow.value.isError shouldBe true
+                viewModel.container.stateFlow.value.errorMessage shouldBe "계산 오류"
+            }
+
+            Then("에러 스낵바가 표시되어야 한다") {
+                viewModel.container.sideEffectFlow.test {
+                    val effect = awaitItem()
+                    effect shouldBe CalculatorContract.SideEffect.ShowSnackBar(
+                        "계산할 수 없는 수식입니다."
+                    )
+                    cancelAndIgnoreRemainingEvents()
+                }
             }
         }
     }
